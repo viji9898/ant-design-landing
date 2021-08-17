fe056333745165723034a22ddf9cec59b0a0f476
Viji <71524056+viji9898@users.noreply.github.com>
2021-08-17 21:18:11 +0100

    inital commit

---
+++ src/Home/data.source.js
@@ -0,0 +1,31 @@
+import React from 'react';
+export const Feature10DataSource = {
+  wrapper: { className: 'home-page-wrapper content1-wrapper' },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children:
+      'https://mylaunchfreebiebucket.s3.eu-west-2.amazonaws.com/svg-1.svg',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: {
+    className: 'content1-title',
+    children: (
+      <span>
+        <p>THE PLATFORM</p>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content ksezawc5qvl-editor_css',
+    children: (
+      <span>
+        <p>
+          A platform that brings together instructors from mobility, strenght,
+          mindfulness and nutrition. Content that is instructional and
+          educational. Goal of keeping the client consistent and accountable.
+        </p>
+      </span>
+    ),
+  },
+};
